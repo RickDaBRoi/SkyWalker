c312a731ffeed6729d8592bbfb30a5dbfd5eb824
RickDaBRoi <32529924+RickDaBRoi@users.noreply.github.com>
2018-08-31 13:32:26 -0300

    Update Administrador.java

--- SkyWalker/src/modelo/Administrador.java
+++ SkyWalker/src/modelo/Administrador.java
@@ -1,6 +1,7 @@
 package modelo;
 
 import javax.persistence.Column;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,7 +11,7 @@ import javax.persistence.Id;
 public class Administrador {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long idAdministrador;
+	private long idAdministrador;
 	@Column(nullable = false)
 	private String nome;
 	@Column(unique = true, nullable = false)
@@ -20,11 +21,61 @@ public class Administrador {
 	@Column(nullable = false)
 	private String senha;
 
+	public Administrador() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + (int) (idAdministrador ^ (idAdministrador >>> 32));
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Administrador other = (Administrador) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (idAdministrador != other.idAdministrador)
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
+		return true;
+	}
+
 	public Long getIdAdministrador() {
 		return idAdministrador;
 	}
 
-	public void setIdAdministrador(Long idAdministrador) {
+	public void setIdAdministrador(long idAdministrador) {
 		this.idAdministrador = idAdministrador;
 	}
 
@@ -59,5 +110,4 @@ public class Administrador {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-
 }
