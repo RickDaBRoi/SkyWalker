c440ab7a466425ef1f080c8f09e2c3de58267a67
RickDaBRoi <32529924+RickDaBRoi@users.noreply.github.com>
2018-08-31 13:34:17 -0300

    Update Cartao.java

--- SkyWalker/src/modelo/Cartao.java
+++ SkyWalker/src/modelo/Cartao.java
@@ -15,7 +15,7 @@ public class Cartao {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long idCartao;
+	private long idCard;
 	@Column(nullable = false, unique = true)
 	private int numCartao;
 	@Column(nullable = false)
@@ -24,12 +24,50 @@ public class Cartao {
 	@Column(nullable = false)
 	private int codSeguranca;
 
-	public Long getIdCartao() {
-		return idCartao;
+	public Cartao() {
+		super();
 	}
 
-	public void setIdCartao(Long idCartao) {
-		this.idCartao = idCartao;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + codSeguranca;
+		result = prime * result + (int) (idCard ^ (idCard >>> 32));
+		result = prime * result + numCartao;
+		result = prime * result + ((validade == null) ? 0 : validade.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cartao other = (Cartao) obj;
+		if (codSeguranca != other.codSeguranca)
+			return false;
+		if (idCard != other.idCard)
+			return false;
+		if (numCartao != other.numCartao)
+			return false;
+		if (validade == null) {
+			if (other.validade != null)
+				return false;
+		} else if (!validade.equals(other.validade))
+			return false;
+		return true;
+	}
+
+	public Long getIdCard() {
+		return idCard;
+	}
+
+	public void setIdCard(long idCard) {
+		this.idCard = idCard;
 	}
 
 	public int getNumCartao() {
