4085a76cae6713d9526ee57b0934ddad9bc90c3f
RickDaBRoi <32529924+RickDaBRoi@users.noreply.github.com>
2018-08-31 13:36:30 -0300

    Update DependenteCliente.java

--- SkyWalker/src/modelo/DependenteCliente.java
+++ SkyWalker/src/modelo/DependenteCliente.java
@@ -12,9 +12,10 @@ import javax.persistence.TemporalType;
 
 @Entity
 public class DependenteCliente {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long idDependente;
+	private long idDependente;
 	@Column(nullable = false)
 	private String nome;
 	@Column(nullable = false)
@@ -26,12 +27,70 @@ public class DependenteCliente {
 	private String login;
 	@Column(nullable = false)
 	private String senha;
+	
 
+	public DependenteCliente() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
+		result = prime * result + (int) (idDependente ^ (idDependente >>> 32));
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DependenteCliente other = (DependenteCliente) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (dataNascimento == null) {
+			if (other.dataNascimento != null)
+				return false;
+		} else if (!dataNascimento.equals(other.dataNascimento))
+			return false;
+		if (idDependente != other.idDependente)
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
+		return true;
+	}
+	
 	public Long getIdDependente() {
 		return idDependente;
 	}
 
-	public void setIdDependente(Long idDependente) {
+	public void setIdDependente(long idDependente) {
 		this.idDependente = idDependente;
 	}
 
