3aaba33e36cfa200029f399aa6d21b9cbeeeee09
RickDaBRoi <32529924+RickDaBRoi@users.noreply.github.com>
2018-08-31 13:35:32 -0300

    Update Conteudo.java

--- SkyWalker/src/modelo/Conteudo.java
+++ SkyWalker/src/modelo/Conteudo.java
@@ -1,7 +1,6 @@
 package modelo;
 
 import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -29,6 +28,67 @@ public class Conteudo {
 	private Date anoLancamento;
 	@Column(nullable = false)
 	private double tempoDuracao;
+
+	public Conteudo() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((anoLancamento == null) ? 0 : anoLancamento.hashCode());
+		result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+		result = prime * result + ((idConteudo == null) ? 0 : idConteudo.hashCode());
+		result = prime * result + indicacao;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(tempoDuracao);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conteudo other = (Conteudo) obj;
+		if (anoLancamento == null) {
+			if (other.anoLancamento != null)
+				return false;
+		} else if (!anoLancamento.equals(other.anoLancamento))
+			return false;
+		if (genero == null) {
+			if (other.genero != null)
+				return false;
+		} else if (!genero.equals(other.genero))
+			return false;
+		if (idConteudo == null) {
+			if (other.idConteudo != null)
+				return false;
+		} else if (!idConteudo.equals(other.idConteudo))
+			return false;
+		if (indicacao != other.indicacao)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (Double.doubleToLongBits(tempoDuracao) != Double.doubleToLongBits(other.tempoDuracao))
+			return false;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
+	}
 
 	public Long getIdConteudo() {
 		return idConteudo;
