59e1359c92a5ce2f8febcd4f298e02c8936ca26a
RickDaBRoi <32529924+RickDaBRoi@users.noreply.github.com>
2018-08-31 13:41:59 -0300

    Update Plano.java

--- SkyWalker/src/modelo/Plano.java
+++ SkyWalker/src/modelo/Plano.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 public class Plano {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long idPlano;
+	private long idPlano;
 	@Column(nullable = false)
 	private String nome;
 	@Column(nullable = false)
@@ -18,11 +18,52 @@ public class Plano {
 	@Column(nullable = false)
 	private String especificacao;
 
-	public Long getIdPlano() {
+	public Plano() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((especificacao == null) ? 0 : especificacao.hashCode());
+		result = prime * result + (int) (idPlano ^ (idPlano >>> 32));
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + valor;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Plano other = (Plano) obj;
+		if (especificacao == null) {
+			if (other.especificacao != null)
+				return false;
+		} else if (!especificacao.equals(other.especificacao))
+			return false;
+		if (idPlano != other.idPlano)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (valor != other.valor)
+			return false;
+		return true;
+	}
+
+	public long getIdPlano() {
 		return idPlano;
 	}
 
-	public void setIdPlano(Long idPlano) {
+	public void setIdPlano(long idPlano) {
 		this.idPlano = idPlano;
 	}
 
