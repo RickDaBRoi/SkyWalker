5c4df260d05d5f6318ea28ae88a379fe514e0ca5
RickDaBRoi <32529924+RickDaBRoi@users.noreply.github.com>
2018-08-31 13:40:22 -0300

    Update Pagamento.java

--- SkyWalker/src/modelo/Pagamento.java
+++ SkyWalker/src/modelo/Pagamento.java
@@ -12,15 +12,47 @@ import javax.persistence.TemporalType;
 public class Pagamento {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long idPagamento;
+	private long idPagamento;
 	@Temporal(TemporalType.DATE)
 	private Date dataPagamento;
+
+	public Pagamento() {
+		super();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataPagamento == null) ? 0 : dataPagamento.hashCode());
+		result = prime * result + (int) (idPagamento ^ (idPagamento >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pagamento other = (Pagamento) obj;
+		if (dataPagamento == null) {
+			if (other.dataPagamento != null)
+				return false;
+		} else if (!dataPagamento.equals(other.dataPagamento))
+			return false;
+		if (idPagamento != other.idPagamento)
+			return false;
+		return true;
+	}
 
 	public Long getIdPagamento() {
 		return idPagamento;
 	}
 
-	public void setIdPagamento(Long idPagamento) {
+	public void setIdPagamento(long idPagamento) {
 		this.idPagamento = idPagamento;
 	}
 
